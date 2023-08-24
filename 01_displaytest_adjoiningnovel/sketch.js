06630a1f3dc1c9b0c96a0aedd4f73166b0d0b1fe
afieldguide <143110308+afieldguide@users.noreply.github.com>
2023-08-24 11:43:21 -0400

    Add files via upload

---
+++ 01_displaytest_adjoiningnovel/sketch.js
@@ -0,0 +1,39 @@
+let r = 0;
+let img;
+
+function preload() {
+  img = loadImage('icon.png');
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+}
+
+function draw() {
+  background(0);
+  
+  rect(25,25,width-50,height-50);
+  
+    for(let i = 50; i < width; i+=50){
+      for(let j = 50; j < height; j+=50){
+        push();
+        translate(i,j);
+        rotate(r);
+        scale(5);
+        triangle(5,5, -5,5, 0,-5);
+        
+        if(dist(i,j,mouseX,mouseY)<15){
+          tint(255);
+        } else { tint(0,0,255); }
+        
+        image(img,-10/2,-10/2,10,10);
+        pop();
+      }
+    }
+  
+}
+
+function mouseReleased() {
+  r += 45;
+}
